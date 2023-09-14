46406ba44a3b982c02d3c65263b1453a035aeeb5
onlyfatih <farslan0699@gmail.com>
2023-09-14 10:44:58 +0300

    Duzenlendi

--- src/test/java/runners/runner.java
+++ src/test/java/runners/runner.java
@@ -1,22 +1,2 @@
-package runners;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(
-        features = {"src/test/java/features"}, // Test senaryolarının (feature dosyalarının) yolunu belirtir.
-        glue = {"stepDefinitions", "util"},    // Senaryoların adım tanımlama kodlarının bulunduğu paketleri belirtir.
-        tags = "@SearchBusStop",                             // Hangi senaryoların çalıştırılacağını etiketle belirler.
-        plugin = {
-                "summary",                      // Özet raporunu konsola basar.
-                "pretty",                       // Test sonuçlarını daha okunabilir bir biçimde konsola basar.
-                "html:Reports/CucumberReport/Reports.html", // HTML raporunu belirtilen yere oluşturur.
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-        }
-)
-//@Listeners({ExtentITestListenerClassAdapter.class})
-public class runner extends AbstractTestNGCucumberTests {
-    // AbstractTestNGCucumberTests sınıfını genişleterek Cucumber testlerini TestNG ile çalıştırmamızı sağlar.
+package runners;public class runner {
 }
-
-
